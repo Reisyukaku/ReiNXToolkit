a670a4e69442e2e806d661a2d846456ad77050c6
Rei <asterma@yahoo.com>
2018-09-16 14:38:34 -0400

    article 13 compliant memes

---
+++ README.md
@@ -0,0 +1,35 @@
+# ReiNX Toolkit
+[![License (GPL version 3)](https://img.shields.io/badge/license-GNU%20GPL%20version%203-red.svg?style=flat-square)](http://opensource.org/licenses/GPL-3.0)
+
+*A toolkit to be used with the ReiNX CFW*
+
+
+**Compiling:**
+
+You'll need latest devkitpro and switch-sdl. You can install dependencies by running `pacman -S switch-sdl2 switch-sdl2_mixer switch-sdl2_gfx switch-sdl2_image switch-sdl2_ttf`.
+
+
+**Features:**
+
+ * Update the ReiNX CFW
+
+ * Toggle AutoRCM
+ 
+ * Kip manager
+
+ * Dump NAND/Cal0/Boot0/Boot1
+
+ * Convert splash screen given a jpg/png/bmp to splash.bin
+ 
+ * Reboot / shutdown 
+
+
+**Credits:**
+ 
+ Calypso and Crusatyr for NAND functions
+ 
+ fgsds for utilies code
+ 
+ neonsea for kip manager and various other fixes/contributions
+ 
+ The ReiSwitched community for being my beta testers!
--- source/UI.cpp
+++ source/UI.cpp
@@ -1,9 +1,26 @@
+/*
+* ReiNX Toolkit
+* Copyright (C) 2018  Team ReiSwitched
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <switch.h>
 #include "Net/Request.hpp"
 #include "Tools/autorcm.hpp"
 #include "Tools/kipmanager.hpp"
 #include "Utils/unzip_utils.hpp"
-#include "Utils/json11.hpp"
 #include "FS.hpp"
 #include "UI.hpp"
 #include "Power.hpp"
